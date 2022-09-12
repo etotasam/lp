57964f8409a1d2cf37bd618c4a3c9f43423c2337
etotasam <spanish2007@gmail.com>
2022-09-13 07:06:56 +0900

    topコンポーネントとheaderコンポーネントをひとつにまとめた
    storybookを使ってコンポーネントを作成するのに出来る限り presentation-component にするべきなので・・・

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 //! components
 import { Header } from "./components/Header";
-import { Top } from "./components/Top";
 import { About } from "./components/About";
 import { Tours } from "./components/Tours";
 import { Footer } from "./components/Footer";
@@ -16,7 +15,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Top />
       {/* <About /> */}
       {/* <Tours /> */}
       <Footer />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,19 +3,20 @@ import React from "react";
 import styles from "./header.module.scss";
 import navStyles from "./nav.module.scss";
 //! hooks
-import { useQueryState } from "@/hooks/useQueryState";
 import { useTitleAnimation } from "./hooks/useTitleAnimation";
 import { useNavAnimation } from "./hooks/useNavAnimation";
+
 export const Header = () => {
   const title = "トルコの旅";
   const navList = ["トルコの魅力", "News", "Tours", "Concept"];
-  const [topContainerEl] = useQueryState<HTMLDivElement>(`ref/topContainer`);
+
+  const containerRef = React.useRef<HTMLDivElement>(null);
 
   //? title
   const titelWrapperRef = React.useRef<HTMLHeadingElement>(null);
   const titleRef = React.useRef<HTMLDivElement>(null);
   const navWrapperRef = React.useRef<HTMLDivElement>(null);
-  useTitleAnimation({ titelWrapperRef, titleRef, navWrapperRef });
+  useTitleAnimation({ titelWrapperRef, titleRef, navWrapperRef, containerRef });
 
   //? nav
   const ulRef = React.useRef<HTMLUListElement>(null);
@@ -28,37 +29,37 @@ export const Header = () => {
   navList.forEach((_, i) => {
     listRef.current[i] = React.createRef<HTMLUListElement>();
   });
-  useNavAnimation({ ulRef, listRef, listInnerDivRef });
+  useNavAnimation({ ulRef, listRef, listInnerDivRef, containerRef });
 
   return (
     <>
-      <header className={`${styles["header"]}`}>
-        <div ref={navWrapperRef} className={`${styles["header-bar"]}`} />
-        {topContainerEl && (
-          <div>
-            <div
-              ref={titelWrapperRef}
-              className={`${styles["title"]}`}
-              onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
-            >
-              <h1 ref={titleRef}>{title}</h1>
-            </div>
+      <div ref={containerRef} className={styles["container"]}>
+        <div className={styles["bg-image"]} />
 
-            {/* //? nav */}
-            <nav className={`${navStyles["nav"]}`}>
-              <ul ref={ulRef} className={`${navStyles["nav-ul"]}`}>
-                {navList.map((text, index) => (
-                  <li key={text} ref={listRef.current[index]}>
-                    <div key={text} className={`${navStyles["nav-list"]}`} ref={listInnerDivRef.current[index]}>
-                      <span className={`${navStyles["nav-list-span"]}`}>{text}</span>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </nav>
+        <header className={`${styles["header"]}`}>
+          <div ref={navWrapperRef} className={`${styles["header-bar"]}`} />
+          <div
+            ref={titelWrapperRef}
+            className={`${styles["title"]}`}
+            onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
+          >
+            <h1 ref={titleRef}>{title}</h1>
           </div>
-        )}
-      </header>
+
+          {/* //? nav */}
+          <nav className={`${navStyles["nav"]}`}>
+            <ul ref={ulRef} className={`${navStyles["nav-ul"]}`}>
+              {navList.map((text, index) => (
+                <li key={text} ref={listRef.current[index]}>
+                  <div key={text} className={`${navStyles["nav-list"]}`} ref={listInnerDivRef.current[index]}>
+                    <span className={`${navStyles["nav-list-span"]}`}>{text}</span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </header>
+      </div>
     </>
   );
 };
